3f6f4b3803e1b85e8d7eec3d1a375670f84dfac6
Simeon Kakpovi <skakpovi@gmail.com>
2016-09-08 15:53:59 -0400

    first commit

---
+++ src/Book.java
@@ -0,0 +1,68 @@
+
+public class Book {
+
+	private String title;
+	private String author;
+	private String description;
+	private double price;
+	private boolean isInStock;
+	
+	
+	public Book(String book_title, String book_author, String book_description, double book_price, boolean book_isInStock){
+		title = book_title;
+		author= book_author;
+		description = book_description;
+		price = book_price;
+		isInStock = book_isInStock;
+	}
+	
+	public void getDisplayText(){
+		System.out.println("Title: " + title);
+		System.out.println("Author: " + author);
+		System.out.println("Description: " + description);
+	}
+	
+	
+	public String getTitle () {
+		return title;
+	}
+	
+	public void setTitle (String new_title) {
+		title = new_title;
+	}
+	
+	public String getAuthor () {
+		return author;
+	}
+	
+	public void setAuthor (String new_author) {
+		author = new_author;
+	}
+	
+	public String getDescription () {
+		return description;
+	}
+	
+	public void setDescription (String new_description) {
+		description = new_description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double new_price) {
+		price = new_price;
+	}
+ 	
+	public boolean getIsInStock(){
+		return isInStock;
+	}
+	
+	public void setIsInStock(boolean status){
+		isInStock = status;
+	}
+	
+	
+	
+}//end Book class
---
+++ src/BookApp.java
@@ -0,0 +1,9 @@
+
+public class BookApp {
+	
+	public static void main(String[] args){
+		Book myBook = new Book("Twilight", "Not Me", "A book about love and sht", 32.122, true);
+		myBook.getDisplayText();
+	}
+
+}
